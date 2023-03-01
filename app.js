2d3aff4f5fe13858aca95b92211922fd47ceeb1c
Kaylee Eichorn <eichornk@flip2.engr.oregonstate.edu>
2023-03-01 15:07:11 -0800

    changed port

--- app.js
+++ app.js
@@ -4,7 +4,7 @@
 // Express
 var express = require('express');   // We are using the express library for the web server
 var app     = express();            // We need to instantiate an express object to interact with the server in our code
-PORT        = 65063;                 // Set a port number at the top so it's easy to change in the future
+PORT        = 65062;                 // Set a port number at the top so it's easy to change in the future
 
 // Database
 var db = require('./db-connector')
