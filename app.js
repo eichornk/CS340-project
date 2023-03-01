efb8f9b88e5830591504b2155aeb10645213bf8b
Abigail Ruff <ruffa@flip1.engr.oregonstate.edu>
2023-03-01 14:41:24 -0800

    Added Folders for Organization

--- app.js
+++ app.js
@@ -4,10 +4,10 @@
 // Express
 var express = require('express');   // We are using the express library for the web server
 var app     = express();            // We need to instantiate an express object to interact with the server in our code
-PORT        = 65063;                 // Set a port number at the top so it's easy to change in the future
+PORT        = 4215;                 // Set a port number at the top so it's easy to change in the future
 
 // Database
-var db = require('./db-connector')
+var db = require('./database/db-connector')
 
 /*
     ROUTES
