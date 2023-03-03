9829bff2b99d92d2fcbae8837dc67cb2ef4f6d2f
Abigail Ruff <ruffa@flip1.engr.oregonstate.edu>
2023-03-02 16:04:18 -0800

    Add Handlebars and Add function

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+// Citation for the following code:
+// Date: 3/2/2023
+// Copied and adapted from OSU GitHub (osu-cs340-ecampus) project (nodejs-starter-app)
+// Source URL: https://github.com/osu-cs340-ecampus/nodejs-starter-app
 /*
     SETUP for a simple web app 
 */
@@ -7,7 +11,10 @@ var exphbs = require('express-handlebars');     // Import express-handlebars
 app.engine('.hbs', engine({extname: ".hbs"}));  // Create an instance of the handlebars engine to process templates
 app.set('view engine', '.hbs');                 // Tell express to use the handlebars engine whenever it encounters a *.hbs file.
 var express = require('express');   // We are using the express library for the web server
-var app     = express();            // We need to instantiate an express object to interact with the server in our code
+var app     = express(); 
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+app.use(express.static('public'))           // We need to instantiate an express object to interact with the server in our code
 PORT        = 65062;                 // Set a port number at the top so it's easy to change in the future
 
 // Database
@@ -20,12 +27,49 @@ app.get('/', function(req, res)
 {
     res.render('index');                    // Note the call to render() and not send(). Using render() ensures the templating engine
 });                                         // will process this file, before sending the finished HTML to the client.
+//Function Reads Chapter Philanthropies table
 app.get('/chapterphilos', function (req, res) {
     let query1 = "SELECT * FROM Chapter_Philanthropies;";
     db.pool.query(query1, function (error, rows, fields) {
         res.render('chapter_philanthropies', { data: rows });
     })
 });
+
+//Function adds Chapter Philanthropy
+app.post('/add-chapter-philanthropy-ajax', function(req, res) 
+{
+    // Capture the incoming data and parse it back to a JS object
+    let data = req.body;
+    // Create the query and run it on the database
+    query1 = `INSERT INTO Chapter_Philanthropies (chapter_philanthropy_id, philanthropy_role, event_id, chapter_id) VALUES ('${data.chapter_philanthropy_id}', '${data.philanthropy_role}', ${data.event_id}, ${data.chapter_id})`;
+    db.pool.query(query1, function(error, rows, fields){
+        // Check to see if there was an error
+        if (error) {
+            // Log the error to the terminal so we know what went wrong, and send the visitor an HTTP response 400 indicating it was a bad request.
+            console.log(error)
+            res.sendStatus(400);
+        }
+        else
+        {
+            // If there was no error, perform a SELECT * on bsg_people
+            query2 = `SELECT * FROM Chapter_Philanthropies;`;
+            db.pool.query(query2, function(error, rows, fields){
+                // If there was an error on the second query, send a 400
+                if (error) {
+                    // Log the error to the terminal so we know what went wrong, and send the visitor an HTTP response 400 indicating it was a bad request.
+                    console.log(error);
+                    res.sendStatus(400);
+                }
+                // If all went well, send the results of the query back.
+                else
+                {
+                    res.send(rows);
+                }
+            })
+        }
+    })
+});
+
 
 /*
     LISTENER
---
+++ public/js/add_chapterphilo.js
@@ -0,0 +1,99 @@
+// Citation for the following code:
+// Date: 3/2/2023
+// Copied and adapted from OSU GitHub (osu-cs340-ecampus) project (nodejs-starter-app)
+// Source URL: https://github.com/osu-cs340-ecampus/nodejs-starter-app
+
+// Get the objects we need to modify
+let addChapterPhilanthropyForm = document.getElementById('add-chapter-philanthropy-form-ajax');
+
+// Modify the objects we need
+addChapterPhilanthropyForm.addEventListener("submit", function (e) {
+    
+    // Prevent the form from submitting
+    e.preventDefault();
+
+    // Get form fields we need to get data from
+    //let inputChapterPhilanthropyId = document.getElementById("input-chapter-philanthropy-id");
+    let inputPhilanthropyRole = document.getElementById("input-philanthropy-role");
+    let inputEventId = document.getElementById("input-event-id");
+    let inputChapterId = document.getElementById("input-chapter-id");
+
+    // Get the values from the form fields
+    //let ChapterPhilanthropyIdValue = inputChapterPhilanthropyId.value;
+    let PhilanthropyRoleValue = inputPhilanthropyRole.value;
+    let EventIdValue = inputEventId.value;
+    let ChapterIdValue = inputChapterId.value;
+
+    // Put our data we want to send in a javascript object
+    let data = {
+        //chapter_philanthropy_id: ChapterPhilanthropyIdValue,
+        philanthropy_role: PhilanthropyRoleValue,
+        event_id: EventIdValue,
+        chapter_id: ChapterIdValue
+    }
+    
+    // Setup our AJAX request
+    var xhttp = new XMLHttpRequest();
+    xhttp.open("POST", "/add-chapter-philanthropy-ajax", true);
+    xhttp.setRequestHeader("Content-type", "application/json");
+
+    // Tell our AJAX request how to resolve
+    xhttp.onreadystatechange = () => {
+        if (xhttp.readyState == 4 && xhttp.status == 200) {
+
+            // Add the new data to the table
+            addRowToTable(xhttp.response);
+
+            // Clear the input fields for another transaction
+            //inputChapterPhilanthropyId.value = '';
+            inputPhilanthropyRole.value = '';
+            inputEventId.value = '';
+            inputChapterId.value = '';
+        }
+        else if (xhttp.readyState == 4 && xhttp.status != 200) {
+            console.log("There was an error with the input.")
+        }
+    }
+
+    // Send the request and wait for the response
+    xhttp.send(JSON.stringify(data));
+
+})
+
+
+// Creates a single row from an Object representing a single record from 
+// Chapter_Philanthropies
+addRowToTable = (data) => {
+
+    // Get a reference to the current table on the page and clear it out.
+    let currentTable = document.getElementById("chapter-philanthropies-table");
+
+    // Get the location where we should insert the new row (end of table)
+    let newRowIndex = currentTable.rows.length;
+
+    // Get a reference to the new row from the database query (last object)
+    let parsedData = JSON.parse(data);
+    let newRow = parsedData[parsedData.length - 1]
+
+    // Create a row and 4 cells
+    let row = document.createElement("TR");
+    let ChapterPhiloIdCell = document.createElement("TD");
+    let PhilanthropyRoleCell = document.createElement("TD");
+    let EventIdCell = document.createElement("TD");
+    let ChapterIdCell = document.createElement("TD");
+
+    // Fill the cells with correct data
+    ChapterPhiloIdCell.innerText = newRow.chapter_philanthropy_id;
+    PhilanthropyRoleCell.innerText = newRow.philanthropy_role;
+    EventIdCell.innerText = newRow.event_id;
+    ChapterIdCell.innerText = newRow.chapter_id;
+
+    // Add the cells to the row 
+    row.appendChild(ChapterPhiloIdCell);
+    row.appendChild(PhilanthropyRoleCell);
+    row.appendChild(EventIdCell);
+    row.appendChild(ChapterIdCell);
+    
+    // Add the row to the table
+    currentTable.appendChild(row); 
+}
