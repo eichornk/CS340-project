357b444744046a82a64c3eedad1f4638bb19a266
Kaylee Eichorn <eichornk@flip2.engr.oregonstate.edu>
2023-03-06 15:04:51 -0800

    Added

--- app.js
+++ app.js
@@ -8,51 +8,87 @@
 */
 
 // Express
-var express = require('express');   // We are using the express library for the web server
+var express = require('express');                           // We are using the express library for the web server
 var app     = express(); 
 
 // app.js - SETUP section 
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
-app.use(express.static('public'))           // We need to instantiate an express object to interact with the server in our code
+app.use(express.static('public'))                           // We need to instantiate an express object to interact with the server in our code
 
-PORT        = 65062;                 // Set a port number at the top so it's easy to change in the future
+PORT        = 65062;                                        // Set a port number at the top so it's easy to change in the future
 
 // Database
 var db = require('./database/db-connector')
 
 // Handlebars
 const { engine } = require('express-handlebars');
-var exphbs = require('express-handlebars');     // Import express-handlebars
-app.engine('.hbs', engine({extname: ".hbs"}));  // Create an instance of the handlebars engine to process templates
-app.set('view engine', '.hbs');                 // Tell express to use the handlebars engine whenever it encounters a *.hbs file.
+var exphbs = require('express-handlebars');                 // Import express-handlebars
+app.engine('.hbs', engine({extname: ".hbs"}));              // Create an instance of the handlebars engine to process templates
+app.set('view engine', '.hbs');                             // Tell express to use the handlebars engine whenever it encounters a *.hbs file.
 
 /*
     ROUTES
 */
+
+// Route to index 
 app.get('/', function(req, res)
-    {
-    res.render('index');                    // Note the call to render() and not send(). Using render() ensures the templating engine
-    });                                         // will process this file, before sending the finished HTML to the client.
+{
+    res.render('index');                                    // Note the call to render() and not send(). Using render() ensures the templating engine
+});                                                         // will process this file, before sending the finished HTML to the client.
 
-app.get('/chapterphilo', function (req, res)
-    {
-    let query1 = "SELECT * FROM Chapter_Philanthropies;";
-    db.pool.query(query1, function (error, rows, fields) {
-        res.render('chapterphilo', { data: rows });
-        })
-    });
-
-/*
-// Function Reads Chapter Philanthropies table
+// Route to chapterphilo 
 app.get('/chapterphilo', function (req, res)
 {
     let query1 = "SELECT * FROM Chapter_Philanthropies;";
     db.pool.query(query1, function (error, rows, fields) {
-        res.render('chapterphilo', { data: rows });             //'Chapter_Philanthropy vs. chapterphilo? 
-    })
+        res.render('chapterphilo', { data: rows });         // Here I changed 'Chapter_Philanthropy' to 'chapterphilo' and the routing process worked 
+        })
 });
-*/
+
+app.post('/add-chapterphilo-ajax', function(req, res){
+    // Capture the incoming data and parse it back to a JS object
+    let data = req.body;
+
+    // Capture NULL values - I think we can remove this section if they have to pick a value 
+    let philanthropyRole = parseInt(data['input-philanthropy-role']);
+    if (isNaN(philanthropyRole))                            // If philanthropyRole is nothing it will replace it with NULL. Not 100% sure if these are
+    {                                                       // allowed to be NULL values so I will look into this later 
+        philanthropyRole = 'NULL'                              
+    }
+
+    let eventID = parseInt(data['input-event-id']);
+    if (isNaN(eventID))
+    {
+        eventID = 'NULL'
+    }
+
+    let chapterID = parseInt(data['input-chapter-id']);
+    if (isNaN(chapterID))
+    {
+        chapterID = 'NULL'
+    }
+
+    // Create the query and run it on the database
+    query1 = `INSERT INTO Chapter_Philanthropies (chapter_philanthropy_id, philanthropy_role, event_id, chapter_id) VALUES ('${chapter_philanthropy_id}', '${philanthropyRole}', ${eventID}, ${chapterID})`;
+    db.pool.query(query1, function(error, rows, fields){
+
+        // Check to see if there was an error
+        if (error) {
+
+            // Log the error to the terminal so we know what went wrong, and send the visitor an HTTP response 400 indicating it was a bad request.
+            console.log(error)
+            res.sendStatus(400);
+        }
+
+        // If there was no error, we redirect back to our root route, which automatically runs the SELECT * FROM bsg_people and
+        // presents it on the screen
+        else
+        {
+            res.redirect('/');
+        }
+    })
+})
 
 /*
     LISTENER
