a54b65e775e11aee0d8de60a9a54cd6c6bb432db
Kaylee Eichorn <eichornk@flip2.engr.oregonstate.edu>
2023-03-08 13:53:28 -0800

    Added add_chapter.js

---
+++ public/js/add_chapter.js
@@ -0,0 +1,119 @@
+
+// Get the objects we need to modify
+let addChapterPhilanthropyForm = document.getElementById('add-chapter-form-ajax');
+
+// Modify the objects we need
+addChapterPhilanthropyForm.addEventListener("submit", function (e) {
+    
+    // Prevent the form from submitting
+    e.preventDefault();
+
+    // Get form fields we need to get data from
+    //let inputChapterPhilanthropyId = document.getElementById("input-chapter-philanthropy-id");
+    let inputChapterName = document.getElementById("input-chapter-name");
+    let inputNickname = document.getElementById("input-nickname");
+    let inputColors = document.getElementById("input-colors");
+    let inputPhilanthropy = document.getElementById("input-philanthropy");
+    let inputHoused = document.getElementById("input-housed");
+    let inputAddress = document.getElementById("input-address");
+    let inputChapterId = document.getElementById("input-chapter-id");
+
+    // Get the values from the form fields
+    //let ChapterPhilanthropyIdValue = inputChapterPhilanthropyId.value;
+    let ChapterNameValue = inputChapterName.value;
+    let NicknameValue = inputNickname.value;
+    let ColorsValue = inputColors.value;
+    let PhilanthropyValue = inputPhilanthropy.value;
+    let HousedValue = inputHoused.value;
+    let AddressValue = inputAddress.value;
+    let CouncilIdValue = inputChapterId.value;
+
+    // Put our data we want to send in a javascript object
+    let data = {
+        //chapter_philanthropy_id: ChapterPhilanthropyIdValue,
+        chapter_name: ChapterNameValue,
+        nickname: NicknameValue,
+        colors: ColorsValue,
+        philanthropy: PhilanthropyValue,
+        housed: HousedValue,
+        address: AddressValue,
+        council_id: CouncilIdValue
+    }
+    
+    // Setup our AJAX request
+    var xhttp = new XMLHttpRequest();
+    xhttp.open("POST", "/add-chapter-ajax", true);
+    xhttp.setRequestHeader("Content-type", "application/json");
+
+    // Tell our AJAX request how to resolve
+    xhttp.onreadystatechange = () => {
+        if (xhttp.readyState == 4 && xhttp.status == 200) {
+
+            // Add the new data to the table
+            addRowToTable(xhttp.response);
+
+            // Clear the input fields for another transaction
+            inputChapterName.value = '';
+            inputNickname.value = '';
+            inputColors.value = '';
+            inputPhilanthropy.value = '';
+            inputHoused.value = '';
+            inputAddress.value = '';
+            inputChapterId.value = '';
+        }
+        else if (xhttp.readyState == 4 && xhttp.status != 200) {
+            console.log("There was an error with the input.")
+        }
+    }
+
+    // Send the request and wait for the response
+    xhttp.send(JSON.stringify(data));
+    location.reload();                        // Reloads the website after user inputs data 
+})
+
+
+// Creates a single row from an Object representing a single record from 
+// Chapter_Philanthropies
+addRowToTable = (data) => {
+
+    // Get a reference to the current table on the page and clear it out.
+    let currentTable = document.getElementById("chapter-table");                     // fix here 
+
+    // Get the location where we should insert the new row (end of table)
+    let newRowIndex = currentTable.rows.length;
+
+    // Get a reference to the new row from the database query (last object)
+    let parsedData = JSON.parse(data);
+    let newRow = parsedData[parsedData.length - 1]
+
+    // Create a row and 4 cells
+    let row = document.createElement("TR");
+    let ChapterNameCell = document.createElement("TD");
+    let NicknameCell = document.createElement("TD");
+    let ColorsCell = document.createElement("TD");
+    let PhilanthropyCell = document.createElement("TD");
+    let HousedCell = document.createElement("TD");
+    let AddressCell = document.createElement("TD");
+    let ChapterIdCell = document.createElement("TD");
+
+    // Fill the cells with correct data
+    ChapterNameCell.innerText = newRow.chapter_name;
+    NicknameCell.innerText = newRow.nickname;
+    ColorsCell.innerText = newRow.colors;
+    PhilanthropyCell.innerText = newRow.philanthropy;
+    HousedCell.innerText = newRow.housed;
+    AddressCell.innerText = newRow.address;
+    ChapterIdCell.innerText = newRow.chapter_id;
+
+    // Add the cells to the row 
+    row.appendChild(ChapterNameCell);
+    row.appendChild(NicknameCell);
+    row.appendChild(ColorsCell);
+    row.appendChild(PhilanthropyCell);
+    row.appendChild(HousedCell);
+    row.appendChild(AddressCell);
+    row.appendChild(ChapterIdCell);
+    
+    // Add the row to the table
+    currentTable.appendChild(row); 
+}
